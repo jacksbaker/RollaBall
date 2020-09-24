0ecab84da7cb5a876b645aa7edef5402205efd4f
JackBaker1 <163625@chrome.escg.ac.uk>
2020-09-24 16:00:28 +0100

    Fixed movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 0;
 
     private Rigidbody rb;
-
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
         rb = GetComponent<Rigidbody>();
+
     }
 
-    private void OnMove(InputValue movementValue)
+    private void Onmove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+
     }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
         rb.AddForce(movement * speed);
     }
+
 }
